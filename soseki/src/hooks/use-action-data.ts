d457dcfb55268907d9768204de3a0cd5b1c6a2e5
tai-kun <taikun.dev@gmail.com>
2026-06-17 16:23:44 +0900

    feat: ユーティリティー型を追加

--- soseki/src/hooks/use-action-data.ts
+++ soseki/src/hooks/use-action-data.ts
@@ -9,24 +9,38 @@ import useRouterContext from "./use-router-context.js";
  *
  * アクションがリダイレクトを返した場合、フレームワークの内部仕様によって実際の値は `undefined` に置換されるため、型定義においてもその挙動を忠実に表現します。
  */
-type ActionResult<TResult> = TResult extends RedirectResponse ? undefined : TResult;
+type ActionResult<TResult> = Awaited<TResult extends RedirectResponse ? undefined : TResult>;
+
+/**
+ * アクション関数またはデータ型から、最終的に解決されるデータの型を抽出するユーティリティー型です。
+ *
+ * @template TData アクション関数、または解決されるデータの型です。
+ */
+export type FulfilledActionData<TData = unknown> = ActionResult<
+  TData extends (...args: any) => infer TReturn ? TReturn : TData
+>;
+
+/**
+ * {@link useActionData|`useActionData`} カスタムフックが返すオブジェクトの型定義です。
+ *
+ * 非同期処理の進行状況を管理する `NinjaPromise` でラップされた、解決済みのデータ型を表します。
+ *
+ * @template TData アクション関数、またはアクションが返すデータの型定義です。
+ */
+export type ActionData<TData = unknown> = NinjaPromise<FulfilledActionData<TData>>;
 
 /**
  * 現在の階層のルートに紐づくアクション関数の最新の実行結果を購読・取得するためのカスタムフックです。
  *
- * @template TData アクション関数そのものの型、またはアクションが返すことが期待されるデータ構造の型定義です。関数型が渡された場合は、自動的にその戻り値の型（推論結果）が解凍されます。
+ * @template TData アクション関数そのものの型、またはアクションが返すことが期待されるデータ構造の型定義です。関数型が渡された場合は、自動的にその非同期戻り値の型が推論されます。
  * @returns アクションが実行済み、または実行中であれば結果を内包した `NinjaPromise` を返し、一度も実行されていないか対象のアクションが存在しない場合は `undefined` を返します。
  */
-export default function useActionData<TData = unknown>():
-  | NinjaPromise<
-      ActionResult<Awaited<TData extends (...args: any) => infer TReturn ? TReturn : TData>>
-    >
-  | undefined {
+export default function useActionData<TData = unknown>(): ActionData<TData> | undefined {
   const { action } = useRouteContext();
   const actionData = useRouterContext((router) => {
     const { currentEntry, actionDataStore } = router;
     return actionDataStore.get(currentEntry.id)?.get(action!);
   });
 
-  return actionData satisfies NinjaPromise<unknown> | undefined as NinjaPromise<any> | undefined;
+  return actionData satisfies ActionData<unknown> | undefined as ActionData<any> | undefined;
 }
--- soseki/src/hooks/use-loader-data.ts
+++ soseki/src/hooks/use-loader-data.ts
@@ -5,14 +5,30 @@ import useRouteContext from "./use-route-context.js";
 import useRouterContext from "./use-router-context.js";
 
 /**
+ * ローダー関数またはデータ型から、最終的に解決されるデータの型を抽出するユーティリティー型です。
+ *
+ * @template TData ローダー関数、または解決されるデータの型です。
+ */
+export type FulfilledLoaderData<TData = unknown> = Awaited<
+  TData extends (...args: any) => infer TReturn ? TReturn : TData
+>;
+
+/**
+ * {@link useLoaderData|`useLoaderData`} カスタムフックが返すオブジェクトの型定義です。
+ *
+ * 非同期処理の進行状況を管理する `NinjaPromise` でラップされた、解決済みのデータ型を表します。
+ *
+ * @template TData ローダー関数、またはローダーが返すデータの型定義です。
+ */
+export type LoaderData<TData = unknown> = NinjaPromise<FulfilledLoaderData<TData>>;
+
+/**
  * 現在の階層のルートに紐づくローダー関数の実行結果を購読・取得するためのカスタムフックです。
  *
- * @template TData ローダー関数そのものの型、またはローダーが返すことが期待されるデータ構造の型定義です。関数型が渡された場合は、自動的にその非同期戻り値の型（非同期の解凍結果）が推論されます。
+ * @template TData ローダー関数そのものの型、またはローダーが返すことが期待されるデータ構造の型定義です。関数型が渡された場合は、自動的にその非同期戻り値の型が推論されます。
  * @returns ローダーの実行状態を管理している `NinjaPromise` を返します。
  */
-export default function useLoaderData<TData = unknown>(): NinjaPromise<
-  Awaited<TData extends (...args: any) => infer TReturn ? TReturn : TData>
-> {
+export default function useLoaderData<TData = unknown>(): LoaderData<TData> {
   const { loader } = useRouteContext();
   const loaderData = useRouterContext((router) => {
     const { currentEntry, loaderDataStore } = router;
@@ -22,5 +38,5 @@ export default function useLoaderData<TData = unknown>(): NinjaPromise<
     throw new LoaderDataNotFoundError({ loader });
   }
 
-  return loaderData satisfies NinjaPromise<unknown> as NinjaPromise<any>;
+  return loaderData satisfies LoaderData<unknown> as LoaderData<any>;
 }
